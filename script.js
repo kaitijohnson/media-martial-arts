968d5094d7c6832f76caee13aded4920c31a28a9
robotsintheattic <katherine.r.johnson@gmail.com>
2017-03-02 14:17:45 -0700

    Fully functional with updated title and colors

--- script.js
+++ script.js
@@ -1,17 +1,51 @@
 $(document).ready(function(){
+$('#modal1').modal();
   //get dat button
   $('.clearBtn').click(function(){
     console.log("I am clicked")
     $('#search').val('')
     $('.collection-item').remove()
     $('.selection').empty()
+    $('.collection').removeAttr('style')
+  });
+  $('.clearModalBtn').click(function(){
+    console.log("I am clicked")
+    $('#search').val('')
+    $('.collection-item').remove()
+    $('.selection').empty()
+    $('#winner').remove()
+    $('#scores').remove()
+    $('.collection').removeAttr('style')
   });
   $('.fightBtn').click(function(){
     var bookCompare = $('.selection.book').attr('data')
     console.log('.fightBtn -> click / bookCompare = ', bookCompare);
     var movieCompare = $('.selection.movie').attr('data')
     console.log('.fightBtn -> click / bookCompare = ', movieCompare)
+    var selection = $('.selection.book').text()
+    console.log(selection)
+
+    if(selection.includes('(Nicholas Sparks)')){
+      $('#results').append(`<h4 id="winner">You lose!</h4><br><p id="scores">You have selected a work by Nicholas Sparks. Please improve your taste and try again.</p>`)
+    }
+    else if (bookCompare > movieCompare) {
+      console.log("book wins!")
+      $('#results').append(`<h4 id="winner">The book is better!</h4><br><p id="scores"> The book has an average user rating of ${bookCompare} out of 5. The movie is rated at ${movieCompare}.</p>`)
+    }
+    else if (movieCompare > bookCompare) {
+      console.log("movie wins!")
+      $('#results').append(`<h4 id="winner">The movie is better!</h4><br><p id="scores"> The movie has an average user rating of ${movieCompare} out of 5. The book is rated at ${bookCompare}.</p>`)
+    }
+    else if (bookCompare == movieCompare) {
+      console.log("It's a draw!")
+      $('#results').append(`<h4 id="winner">It's a draw!</h4><br><p id="scores"> Both the movie and the book are rated at ${bookCompare} out of 5.</p>`)
+    }
   })
+  $('#search').keydown(function(event){
+    if(event.keyCode==13){
+      $('.searchBtn').trigger('click');
+    }
+  });
   $('.searchBtn').click(function(){
     var userSearch = $('#search').val();
 
@@ -44,14 +78,10 @@ $(document).ready(function(){
         // }
         //add event listener to the collection element
         $('.collection-item.movie').click(function(event){
-          console.log('.collection-item.move -> click')
+          // console.log('.collection-item.move -> click')
           //movie that was selected
           var movie = event.target;
           //select for the movie box
-          // var selectedMovie = $('.collection-item.movie').text()
-
-
-
           var movieDivs = $('.selection.movie').attr('data', movieRating);
 
           for(var i = 0; i < movieDivs.length; i++){
@@ -91,7 +121,7 @@ $(document).ready(function(){
           // let book = work[i];
           var ratingStr = book['average_rating'];
           // console.log(rating)
-          var bookRating = parseFloat(ratingStr).toFixed(1)
+          var bookRating = parseFloat(ratingStr)
           var author = book['best_book']['author']['name']
           // console.log(author)
           var title = book['best_book']['title']
